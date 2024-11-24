a00bdfdd6911b86dd69297d051eaca5f86b7ae7b
Alice Zhang <102567551+Minuet-Hoax@users.noreply.github.com>
2024-11-24 15:24:18 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -7,30 +7,64 @@ window.addEventListener("load", function() {
 	video.loop = false;
 
 	document.querySelector("#play").addEventListener("click", function() {
-		document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+		console.log("Play Video");
 		video.play();
+		document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
 	});
 
-	document.querySelector("slower").addEventListener("click", function() {
+	document.querySelector("#pause").addEventListener("click", function () {
+        console.log("Pause Video");
+		video.pause();
+    });
+
+	document.querySelector("#slower").addEventListener("click", function() {
 		video.playbackRate *= 0.9;
+		console.log("Slow down video")
+		console.log("Speed is " + video.playbackRate);
 	});
 
-	document.querySelector("mute").addEventListener("click", function() {
+	document.querySelector("#faster").addEventListener("click", function () {
+        video.playbackRate /= 0.9;
+		console.log("Speed up Video")
+        console.log("Speed is " + video.playbackRate);
+    });
+
+	document.querySelector("#skip").addEventListener("click", function () {
+        if (video.currentTime + 10 > video.duration) {
+            video.currentTime = 0;
+        } else {
+            video.currentTime += 10;
+        }
+		console.log("Skip ahead")
+        console.log("Video current time is " + video.currentTime);
+    });
+
+	document.querySelector("#mute").addEventListener("click", function() {
 		if (video.muted == true) {
-			//console log: unmute
+			console.log("Unmute");
 			video.muted = false;
 			this.innerHTML = "Mute";
 		}
 		else {
+			console.log("Mute");
 			video.muted = true;
-			document.querySelector("#mute").innerHTML = "Unmute";
+			this.innerHTML = "Unmute";
 		}
 	});
 
-	document.querySelector("#volumeSlider").addEventListener("change", function() {
-		video.volume = this.value / 100;
-		document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
-	});
+	document.querySelector("#slider").addEventListener("input", function () {
+        video.volume = this.value / 100;
+		console.log("The current value is " + video.volume);
+        document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+    });
+
+    document.querySelector("#vintage").addEventListener("click", function () {
+		video.classList.add("oldSchool");
+    });
+
+    document.querySelector("#orig").addEventListener("click", function () {
+        video.classList.remove("oldSchool");
+    });
 
 });
 
